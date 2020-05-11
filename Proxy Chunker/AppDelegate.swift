c25ba46ece82485b45fcaf04b1595ca6b4987897
phucld <phucld@dwarvesv.com>
2020-05-11 11:00:32 +0700

    Initial Commit

---
+++ Proxy Chunker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Proxy Chunker
+//
+//  Created by phucld on 5/11/20.
+//  Copyright © 2020 phucledien. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
